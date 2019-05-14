b7a40cbe45c5d1d9334548fa542b0ea31d05c50e
Riaz Laskar <riazcool77@gmail.com>
2019-05-14 14:07:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# sms-horizon-balance-check
+Nodejs script to download a udemy.com course (videos only), for personal offline use.
+
+### Installation
+```
+npm install -g udemy-dl
+```
+
+### Usage
+
+Include the username/email and password on the command line using the -u and -p parameters.
+
+```
+udl -u user@domain.com -p $ecRe7w0rd https://www.udemy.com/COURSE_NAME
+```
+
+For information about all available parameters, use the `--help` parameter
+```
+udl --help
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const arequest = require('superagent');
+const cheerio = require('cheerio');
+const yargs = require('yargs');
+const argv = yargs
+    .usage( "Usage: [-u \"username\"] [-p \"password\"]" )
+    .option( "u", { alias: "username", demand: false, describe: "Username in SMS Horizon", type: "string" } )
+    .option( "p", { alias: "password", demand: false, describe: "Password of yor account", type: "string" } )
+    .help( "?" )
+    .alias( "?", "help" )
+    .epilog( "By Riaz Ali Laskar" )
+    .argv;
+
+
+
+let check = function(username, password){
+    const agent = arequest.agent();
+    let login_url = `https://smshorizon.co.in/login-exec.php`;
+    let payload = {'login' : username, 'password': password}
+
+    agent
+        .post(login_url)
+        .type('form')
+        .send(payload)
+        .then((res)=>{
+            var $ = cheerio.load(res.text);
+            if($('.wrong').length){
+                console.log($('.wrong').text())
+            } else {
+                console.log('SMS Horizon Balance:',$('.bal').text().replace('(trans_mtnl)',''));
+            }
+        })
+        .catch((e)=>{
+            console.log(e,'Error !!')
+        })
+}
+
+check(argv.username,argv.password);
